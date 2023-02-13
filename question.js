3b8afaeadf84f14fda9c447a085b16f3f3a5e6c8
Angie <bajor.angelina@gmail.com>
2023-02-13 23:59:08 +0100

    change url

--- question.js
+++ question.js
@@ -1,10 +1,10 @@
 document.getElementById("question-button").addEventListener("click", function() {
-    const firstAnswer = window.confirm("Will you be my valentine?");
+    const firstAnswer = window.confirm("Will you be my Valentine?");
     if (firstAnswer) {
         window.alert("Here we go then! <3");
-        window.location.href = "https://www.youtube.com/watch?v=WiXqXXnoBF8";
+        window.location.href = "https://music.youtube.com/playlist?list=PL2tMgWgIvcWl0EudnDLrNH6XowhpzaG4s";
     } else {
         window.alert("Task failed succesfully!");
-        window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+        window.location.href = "https://www.youtube.com/watch?v=6UQN7wBYfHU";
     }
 })
